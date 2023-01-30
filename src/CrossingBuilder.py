d37e5a825e52e8ce7a92980a512a90329b10b260
Nikita Radeev <rdvnkt@yandex.ru>
2023-01-30 08:59:16 +0700

    common view of crossing

--- src/CrossingBuilder.py
+++ src/CrossingBuilder.py
@@ -1,4 +1,6 @@
 import collections
+
+
 def remove_duplicates(crossings_list):
     uniq_crossings = []
     for crossing in crossings_list:
@@ -6,13 +8,18 @@ def remove_duplicates(crossings_list):
             uniq_crossings.append(crossing)
     return uniq_crossings
     
-
-def _build_crossing_prototypes(columns, depth):
+def build_crossings_ptototypes(data, depth):
+    columns = data.columns
     crossings = [[col] for col in columns]
     for _ in range(depth):
         crossings = [sorted(old_cross + [column]) for old_cross in crossings for column in columns]
-    result = remove_duplicates(crossings)
-    return result
+    prototypes = remove_duplicates(crossings)
+    return prototypes
 
-def build_crossings(data, cross_function, depth):
-    columns = data.columns
+def _get_crossing_name(crossing):
+    crossing_name = '_X_'.join(crossing)
+    return crossing_name
+
+def build_crossings(data, crossing, cross_function):
+    cross_name = _get_crossing_name(crossing)
+    data[cross_name] = cross_function(data, crossing)
