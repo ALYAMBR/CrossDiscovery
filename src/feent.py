087f47d53377ab3307a9f9c0b6e6fdddc33200f9
Nikita Radeev <rdvnkt@yandex.ru>
2024-05-10 22:06:07 +0700

    main frame + demo notebook

--- src/feent.py
+++ src/feent.py
@@ -1,7 +1,7 @@
 from src.ui.main_frame import MainFrame
 
 
-class Feint:
+class Feent:
     def __init__(self, data_X, data_y, eval_func) -> None:
         main_frame = MainFrame(data_X, data_y)
         main_frame.show()
--- src/ui/main_frame.py
+++ src/ui/main_frame.py
@@ -2,28 +2,36 @@
 Compilation of all components together.
 """
 
-from collections import defaultdict
-
 import pandas as pd
 import panel as pn
-from panel import param
+import param
 
+
+class ProcessStorage():
+    def __init__(self) -> None:
+        self.selected_features = []
 
 class MainFrame():
     def __init__(self, data_X, data_y) -> None:
         self.data_X = data_X
         self.data_y = data_y
-        self.process_storage = defaultdict(list)
-        self.tmplt = None
+        self.process_storage = ProcessStorage()
+        self.tmplt = self.init_main_frame()
 
     def init_main_frame(self):
-        if self.tmplt is not None:
-            return self.tmplt
-        
         tmplt = pn.template.GoldenTemplate(
-            title='Feint'
+            title='Feent'
             )
         
+        pn_col_feature_selection = self._init_feature_selection()
+
+        tmplt.main.append(
+            pn_col_feature_selection
+        )
+
+        return tmplt
+    
+    def _init_feature_selection(self):
         class FeatureSelection(param.Parameterized):
             cross_selection = param.ListSelector(
                 default=[],
@@ -37,16 +45,40 @@ class MainFrame():
             }
         )
         text_selected_features = pn.widgets.StaticText(
-            value='Selected features: '
+            value='Selected features: []'
         )
         dataframe_show = pn.widgets.DataFrame(
-            pd.concat([self.data_X.head(10), self.data_y.head(10)], axis=0),
+            pd.concat([self.data_X, self.data_y], axis=1),
             autosize_mode='fit_columns',
             frozen_rows=10,
-            width=800    
+            width=1200    
         )
         def on_click_feature_selection(event):
             selected_features = feature_selection.param.get_param_values()[0][1]
-            self.process_storage.update({'selected_features': selected_features})
-            text_selected_features.value = f'Selected features: {self.process_storage.get('selected_features')}'
-            dataframe_show.value = 
+            self.process_storage.selected_features = selected_features
+            text_selected_features.value = f'Selected features: {self.process_storage.selected_features}'
+            dataframe_show.value = pd.concat(
+                [
+                    self.data_X[self.process_storage.selected_features],
+                    self.data_y
+                ],
+                axis=1
+            )
+
+        button_select_features = pn.widgets.Button(name='👴🏻 Select Features')
+        button_select_features.on_click(on_click_feature_selection)
+    
+        pn_col = pn.Column(
+                selector_param,
+                button_select_features,
+                text_selected_features,
+                dataframe_show,
+                name='Feature Selection'
+            )
+    
+        return pn_col
+    
+    # def _init_
+
+    def show(self):
+        self.tmplt.show()
